1828db2c6c4de749e597246a4e88fc9d4b30713e
Rogelio Aguilar <raguilar27@github.com>
2021-08-20 19:00:57 -0700

    added nav links

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -5,6 +5,7 @@ import { DiCssdeck } from "react-icons/di";
 
 import {
   Container,
+  Span,
   Div1,
   Div2,
   Div3,
@@ -12,6 +13,48 @@ import {
   SocialIcons,
 } from "./HeaderStyles";
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "17px",
+          }}
+        >
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#project">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/raguilar27">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/rogelio-aguilar-52b88617b/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
+);
 
 export default Header;
