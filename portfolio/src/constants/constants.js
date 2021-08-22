2a1a66d64ca78be64378a18a53722a480ccfbaa4
Rogelio Aguilar <raguilar27@github.com>
2021-08-21 22:59:08 -0700

    added projects, timeline, accomplishments

--- portfolio/src/constants/constants.js
+++ portfolio/src/constants/constants.js
@@ -1,57 +1,62 @@
 export const projects = [
   {
-    title: "MERN Memories",
-    description:
-      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    title: "Chat App",
+    description: "Realtime Chat Application ",
     image: "/images/1.png",
-    tags: ["Mongo", "Express", "React", "Node"],
-    source: "https://google.com",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://github.com/raguilar27/ChatApp",
     visit: "https://google.com",
     id: 0,
   },
   {
-    title: "E-Commerce",
+    title: "Sudoku Solver",
     description:
-      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+      "This sudoku solver uses python and a backtracking algorithm to find a solution to any solvable sudoku board.",
     image: "/images/2.png",
-    tags: ["React", "JavaScript"],
-    source: "https://google.com",
+    tags: ["Python", "Pygame"],
+    source: "https://github.com/raguilar27/Sudoku-Solver",
     visit: "https://google.com",
     id: 1,
   },
   {
-    title: "WebRTC App",
-    description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: "/images/3.jpg",
-    tags: ["React", "WebRTC"],
-    source: "https://google.com",
+    title: "Recipe App",
+    description: "Recipeies",
+    image: "/images/3.png",
+    tags: ["Html", "CSS", "JavaScript"],
+    source: "https://github.com/raguilar27/Recipe-App",
     visit: "https://google.com",
     id: 2,
   },
   {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/images/4.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
+    title: "DBZ Quiz",
+    description: "Short quiz created for DBZ",
+    image: "/images/4.png",
+    tags: ["Html", "CSS", "JavaScript"],
+    source: "https://github.com/raguilar27/Quiz-App",
     visit: "https://google.com",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
+  { year: 2017, text: "Started my journey learning Java fundemetals" },
+  { year: 2018, text: "Studied Data Structures and Algorithms" },
+  {
+    year: 2019,
+    text: "Researched about Artificial Intelligence and Operating Systems",
+  },
+  {
+    year: 2020,
+    text: "Learned more about developing full stack web applications",
+  },
+  {
+    year: 2021,
+    text: "Graduated from UC Merced with a Bachelor's Degree in Computer Science and Engineering",
+  },
 ];
 
 export const AccomplishmentData = [
-  { number: 20, text: "Open Source Projects" },
-  { number: 1000, text: "Students" },
-  { number: 1900, text: "Github Followers" },
-  { number: 5000, text: "Github Stars" },
+  { number: 9, text: "Projects" },
+  { number: 1, text: "Won Innovate to Grow Competition" },
+  { number: 2, text: "Attended Hackathons" },
 ];
